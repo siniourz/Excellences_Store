4b7d82ca8c70cada93e5f7bf5df57106b19b4dde
siniourz <kazemisina45@gmail.com>
2025-01-28 18:47:30 +0300

    earrings page done

--- Earings-p/earings.js
+++ Earings-p/earings.js
@@ -68,18 +68,17 @@ document.addEventListener("DOMContentLoaded", function() {
             'necklace': 'Necklace',
             'piercing': 'Piercing',
             'contact': 'Contact',
-            'title':'Excellences Store | Necklaces',
-            'necklaces': 'Necklaces',
-            'males': 'For males',
-            'info': 'Elevate your style to its finest version with a men s necklace. This accessory not only adds a unique touch to your look but also enhances your presence, making a powerful and modern statement.',
-            'malesbtt':'explore all necklaces for males',
-            'females': 'For females',
-            'info2':'Transform your look with a necklace designed to highlight your individuality. It’s the perfect way to add sophistication and a powerful statement to any outfit.',
-            'btt2':'Explore all necklaces for Females',
+            'title':'Excellences Store | Earings',
             'payment':'Payment Methods: ',
             'follow':'Follow us on: ',
             'contactus':'contact us at: ',
             'company':'company',
+
+
+            'einfo':'Unleash your inner elegance with our exquisite earrings. Designed to capture attention, these timeless pieces add a touch of sophistication to any outfit, making you shine with grace and style. Perfect for those who seek beauty in every detail.',
+            'ebtt':'Show All The Earrings',
+
+
 
 
 
@@ -88,23 +87,18 @@ document.addEventListener("DOMContentLoaded", function() {
         tr: {
             'excellences': 'Excellences',
             'searchText': 'Ne arıyorsunuz?',
-            'earrings': 'Küpe',
+            'earrings': 'Küpeler',
             'bracelet': 'Bilezik',
             'necklace': 'Kolye',
             'piercing': 'Piercing',
             'contact': 'İletişim',
-            'title':'Excellences Store | Kolye',
-            'necklaces': 'Kolyeler',
-            'males': 'Erkekler için',
-            'info': 'Erkek kolyesiyle tarzınızı en iyi versiyonuna yükseltin. Bu aksesuar sadece görünümünüze benzersiz bir dokunuş katmakla kalmaz, aynı zamanda varlığınızı da güçlendirerek güçlü ve modern bir ifade yaratır.',
-            'malesbtt':'erkekler için tüm kolyeleri keşfedin',
-            'females': 'Kadınlar için',
-            'info2':'Bireyselliğinizi vurgulamak için tasarlanmış bir kolyeyle görünümünüzü dönüştürün. Herhangi bir kıyafete sofistikelik ve güçlü bir ifade katmanın mükemmel yoludur.',
-            'btt2':'Kadınlar için tüm kolyeleri keşfedin',
+            'title':'Excellences Store | Küpe',
             'payment':'ödeme yöntemleri: ',
             'follow':'Bizi takip edin: ',
             'contactus':'bizimle iletişime geçin: ',
             'company':'şirket',
+            'einfo':'Zarif küpelerimizle içinizdeki zarafeti ortaya çıkarın. Dikkat çekmek için tasarlanan bu zamansız parçalar, her kıyafete bir sofistike dokunuş katarak zarafet ve stilinizle parlamanızı sağlar. Her ayrıntıda güzellik arayanlar için mükemmeldir.',
+            'ebtt':'Tüm Küpeleri Göster',
 
 
 
